c71d913c9bd4200c3ccf0caafe5ad6435a9a152d
wendy9910 <wendy991017@gmail.com>
2021-10-13 22:55:14 +0800

    修pad bug

--- Assets/Scripts/Canvas_Pointer/ButtonTransitioner.cs
+++ Assets/Scripts/Canvas_Pointer/ButtonTransitioner.cs
@@ -12,6 +12,7 @@ public class ButtonTransitioner : MonoBehaviour, IPointerEnterHandler, IPointerE
     public static Slider Sslider1, Sslider2, Sslider3;
     public static GameObject Gslider1, Wslider1, Tslider1;
     public static Color HairColor;
+    public GameObject PadA;
 
     public static int HairWidth;
     public static int HairThickness;
@@ -25,66 +26,30 @@ public class ButtonTransitioner : MonoBehaviour, IPointerEnterHandler, IPointerE
 
     private void Awake()
     {
-        m_Image = GetComponent<Image>();
 
-        btns[0] = GameObject.FindGameObjectWithTag("button0");
-        btns[1] = GameObject.FindGameObjectWithTag("button1");
-        btns[2] = GameObject.FindGameObjectWithTag("button2");
-        btns[3] = GameObject.FindGameObjectWithTag("button3");
-        btns[4] = GameObject.FindGameObjectWithTag("button4");
-        btns[5] = GameObject.FindGameObjectWithTag("button5");
-        btns[6] = GameObject.FindGameObjectWithTag("button6");
-        btns[7] = GameObject.FindGameObjectWithTag("button7");
-        btns[8] = GameObject.FindGameObjectWithTag("button8");
-        btns[9] = GameObject.FindGameObjectWithTag("button9");
-        btns[10] = GameObject.FindGameObjectWithTag("button10");
-        btns[11] = GameObject.FindGameObjectWithTag("button11");
-
-        Colorbtns[0] = Color.HSVToRGB(34 / 360.0f, 39 / 100.0f, 87 / 100.0f);
-        Colorbtns[1] = Color.HSVToRGB(60 / 360.0f, 21 / 100.0f, 1);
-        Colorbtns[2] = Color.HSVToRGB(319 / 360.0f, 13 / 100.0f, 99 / 100.0f);
-        Colorbtns[3] = Color.HSVToRGB(0, 0, 0);
-        Colorbtns[4] = Color.HSVToRGB(0, 0f, 68 / 100.0f);
-        Colorbtns[5] = Color.HSVToRGB(25 / 360.0f, 43 / 100.0f, 99 / 100.0f);
-        Colorbtns[6] = Color.HSVToRGB(194 / 360.0f, 29 / 100.0f, 1);
-        Colorbtns[7] = Color.HSVToRGB(296 / 360.0f, 27 / 100.0f, 1);
-        Colorbtns[8] = Color.HSVToRGB(0, 0, 1);
-        Colorbtns[9] = Color.HSVToRGB(78 / 360.0f, 1, 84 / 100.0f);
-        Colorbtns[10] = Color.HSVToRGB(0, 49 / 100.0f, 77 / 100.0f);
-
-        showimg = GameObject.FindGameObjectWithTag("showimage");
-        slider1img = GameObject.FindGameObjectWithTag("slider1img");
-        Gslider1 = GameObject.FindGameObjectWithTag("Gslider1");
-        Wslider1 = GameObject.FindGameObjectWithTag("Wslider1");
-        Tslider1 = GameObject.FindGameObjectWithTag("Tslider1"); 
         //PadB = GameObject.Find("Player/SteamVRObjects/LeftHand/PadB");
         //PadC = GameObject.Find("Player/SteamVRObjects/LeftHand/PadC");
+        PadA = GameObject.Find("Player/SteamVRObjects/LeftHand/PadA");
+        if (PadA.activeSelf == true) FindPadAObject();
 
-        HairColor = Colorbtns[0];
     }
 
     void Start()
     {
-        for (int i = 0; i < 11; i++)
-        {
-            //btns[i].AddComponent<Image>();
-            colorimgs[i] = btns[i].GetComponent<Image>();
-            colorimgs[i].GetComponent<Image>().color = Colorbtns[i];
-        }
-        showimg.GetComponent<Image>();
-        slider1img.GetComponent<Image>();
 
-        //將gameobject slider的slider功能指給程式中的Sslider用
-        Sslider1 = Gslider1.GetComponent<Slider>();
-        Sslider2 = Wslider1.GetComponent<Slider>();
-        Sslider3 = Tslider1.GetComponent<Slider>();
+
 
     }
     public void Update()
     {
-        HairColor = showimg.GetComponent<Image>().color;
-        SliderW();
-        SliderT();
+
+        if (PadA.activeSelf == true)
+        {
+            HairColor = showimg.GetComponent<Image>().color;
+            SliderW();
+            SliderT();
+            Slider1();
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -97,10 +62,24 @@ public class ButtonTransitioner : MonoBehaviour, IPointerEnterHandler, IPointerE
     }
     public void OnPointerDown(PointerEventData eventData)
     {
-        FindTag();
-        //Slider1();
-        SliderW();
-        SliderT();
+        Debug.Log("state=" + CallerPad.state);
+        if (CallerPad.state == 0)
+        {
+            FindPadATag();
+            Slider1();
+            SliderW();
+            SliderT();
+        }
+        if (CallerPad.state == 1) FindPadBTag();
+        if (CallerPad.state == 2) FindPadDTag();
+        if (CallerPad.state == 3)
+        {
+            Debug.Log("Hi");
+            FindPadCTag();
+        }
+        /*if (PadA.activeSelf == true) { 
+           
+        }*/
     }
     public void OnPointerUp(PointerEventData eventData)
     {
@@ -108,10 +87,7 @@ public class ButtonTransitioner : MonoBehaviour, IPointerEnterHandler, IPointerE
     }
     public void OnPointerClick(PointerEventData eventData)
     {
-        //FindTag();
-        //Slider1();
-        SliderW();
-        SliderT();
+
     }
 
     public void Slider1() //調動深淺
@@ -134,7 +110,8 @@ public class ButtonTransitioner : MonoBehaviour, IPointerEnterHandler, IPointerE
         showimg.GetComponent<Image>().color = Color.HSVToRGB(H, Sslider1.value / 100f, V);
         HairColor = showimg.GetComponent<Image>().color;
     }
-    public void FindTag()
+
+    public void FindPadATag()
     {
         //===== PadA-Buttons
         if (gameObject.tag == "button0")
@@ -235,8 +212,12 @@ public class ButtonTransitioner : MonoBehaviour, IPointerEnterHandler, IPointerE
         {
             CreateHair.Redo();
         }
+
+    }
+    public void FindPadBTag()
+    {
         //===== PadB-yes/no (顏色等敏真寫完)
-        else if (gameObject.tag == "PadBYes")
+        if (gameObject.tag == "PadBYes")
         {
             CallerPad.PadAShow();
         }
@@ -244,8 +225,12 @@ public class ButtonTransitioner : MonoBehaviour, IPointerEnterHandler, IPointerE
         {
             CallerPad.PadAShow();
         }
+
+    }
+    public void FindPadCTag()
+    {
         //===== PadC-yes/no (Save等宜均加存檔部分)
-        else if (gameObject.tag == "PadCYes")
+        if (gameObject.tag == "PadCYes")
         {
             CallerPad.PadAShow();
         }
@@ -253,8 +238,12 @@ public class ButtonTransitioner : MonoBehaviour, IPointerEnterHandler, IPointerE
         {
             CallerPad.PadAShow();
         }
+
+    }
+    public void FindPadDTag()
+    {
         //===== PadD-yes/no (宜均要加其他tag上去)
-        else if (gameObject.tag == "PadDYes")
+        if (gameObject.tag == "PadDYes")
         {
             CallerPad.PadAShow();
         }
@@ -262,6 +251,64 @@ public class ButtonTransitioner : MonoBehaviour, IPointerEnterHandler, IPointerE
         {
             CallerPad.PadAShow();
         }
+
     }
+
+    public void FindPadAObject()
+    {
+        m_Image = GetComponent<Image>();
+
+        btns[0] = GameObject.FindGameObjectWithTag("button0");
+        btns[1] = GameObject.FindGameObjectWithTag("button1");
+        btns[2] = GameObject.FindGameObjectWithTag("button2");
+        btns[3] = GameObject.FindGameObjectWithTag("button3");
+        btns[4] = GameObject.FindGameObjectWithTag("button4");
+        btns[5] = GameObject.FindGameObjectWithTag("button5");
+        btns[6] = GameObject.FindGameObjectWithTag("button6");
+        btns[7] = GameObject.FindGameObjectWithTag("button7");
+        btns[8] = GameObject.FindGameObjectWithTag("button8");
+        btns[9] = GameObject.FindGameObjectWithTag("button9");
+        btns[10] = GameObject.FindGameObjectWithTag("button10");
+        btns[11] = GameObject.FindGameObjectWithTag("button11");
+
+        Colorbtns[0] = Color.HSVToRGB(34 / 360.0f, 39 / 100.0f, 87 / 100.0f);
+        Colorbtns[1] = Color.HSVToRGB(60 / 360.0f, 21 / 100.0f, 1);
+        Colorbtns[2] = Color.HSVToRGB(319 / 360.0f, 13 / 100.0f, 99 / 100.0f);
+        Colorbtns[3] = Color.HSVToRGB(0, 0, 0);
+        Colorbtns[4] = Color.HSVToRGB(0, 0f, 68 / 100.0f);
+        Colorbtns[5] = Color.HSVToRGB(25 / 360.0f, 43 / 100.0f, 99 / 100.0f);
+        Colorbtns[6] = Color.HSVToRGB(194 / 360.0f, 29 / 100.0f, 1);
+        Colorbtns[7] = Color.HSVToRGB(296 / 360.0f, 27 / 100.0f, 1);
+        Colorbtns[8] = Color.HSVToRGB(0, 0, 1);
+        Colorbtns[9] = Color.HSVToRGB(78 / 360.0f, 1, 84 / 100.0f);
+        Colorbtns[10] = Color.HSVToRGB(0, 49 / 100.0f, 77 / 100.0f);
+
+        showimg = GameObject.FindGameObjectWithTag("showimage");
+        slider1img = GameObject.FindGameObjectWithTag("slider1img");
+        Gslider1 = GameObject.FindGameObjectWithTag("Gslider1");
+        Wslider1 = GameObject.FindGameObjectWithTag("Wslider1");
+        Tslider1 = GameObject.FindGameObjectWithTag("Tslider1");
+
+        for (int i = 0; i < 11; i++)
+        {
+            //btns[i].AddComponent<Image>();
+            colorimgs[i] = btns[i].GetComponent<Image>();
+            colorimgs[i].GetComponent<Image>().color = Colorbtns[i];
+        }
+
+        showimg.GetComponent<Image>();
+        slider1img.GetComponent<Image>();
+
+        //將gameobject slider的slider功能指給程式中的Sslider用
+        Sslider1 = Gslider1.GetComponent<Slider>();
+        Sslider2 = Wslider1.GetComponent<Slider>();
+        Sslider3 = Tslider1.GetComponent<Slider>();
+
+        HairColor = Colorbtns[0];
+
+    }
+
+
+
 
 }
