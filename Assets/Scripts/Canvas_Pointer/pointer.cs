3fc59f930d225dae0e6a8bf3ba0828d08c863f5e
yiyun12o9 <yoyolee7012@gmail.com>
2021-11-15 22:31:38 +0800

    somechange

--- Assets/Scripts/Canvas_Pointer/pointer.cs
+++ Assets/Scripts/Canvas_Pointer/pointer.cs
@@ -8,19 +8,49 @@ public class pointer : MonoBehaviour
 {
     public float m_DefaultLength = 5.0f;
     public GameObject m_Dot;
+    //public GameObject dot1,dot2;
     public VRInputModule m_InputModule;
+    public static Color PointerColor;
+    public static Color white, red;
+    public static Material nowmaterial,m_red,m_white;
 
     private LineRenderer m_LineRenderer = null;
     // Start is called before the first frame update
     private void Awake()
     {
         m_LineRenderer = GetComponent<LineRenderer>();
+        white = Color.white;
+        red = Color.red;
+        //dot1 = GameObject.Find("Player/RightHand/PR_Pointer/Dot_red");
+        //dot2 = GameObject.Find("Player/RightHand/PR_Pointer/Dot_white");
+        m_red = new Material(Shader.Find("Unlit/Color"));
+        m_red.color=Color.red;
+        m_white = new Material(Shader.Find("Unlit/Color"));
+        m_white.color=Color.white;
+        nowmaterial = m_Dot.GetComponent<MeshRenderer>().material;
     }
 
     // Update is called once per frame
     private void Update()
     {
         UpdateLine();
+        if (CallerPad.state == 1)
+        {
+            m_LineRenderer.SetColors(white, white);
+            /*dot2.SetActive(true);
+            dot1.SetActive(false);
+            m_Dot = dot2;*/
+            nowmaterial = m_white;
+        }
+        else
+        {
+            m_LineRenderer.SetColors(white, red);
+            /*dot2.SetActive(false);
+            dot1.SetActive(true);
+            m_Dot = dot1;*/
+            nowmaterial = m_red;
+        }
+        
     }
     private void UpdateLine()
     {
